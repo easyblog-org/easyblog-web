a70d10d956db543a5acd534f1f1ff3a5103ce8a8
LoverITer <2489868503@qq.com>
2022-03-10 22:42:38 +0800

    feat: init vuetify

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,7 +11,33 @@ module.exports = {
     parser: '@babel/eslint-parser'
   },
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    // allow async-await
+    'generator-star-spacing': 'off',
+    // allow debugger during development
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-trailing-spaces': 'off',
+    indent: 'off',
+    'spaced-comment':'off',
+    'no-new':'off',
+    'key-spacing':'off',
+    'comma-dangle':'off',
+    'eol-last':'off',
+    'quotes':'off',
+    'semi':'off',
+    'keyword-spacing':'off',
+    'space-before-blocks':'off',
+    'comma-spacing':'off',
+    'space-before-function-paren':'off',
+    'space-infix-ops':'off',
+    'no-multiple-empty-lines':'off',
+    'block-spacing':'off',
+    'padded-blocks':'off',
+    'arrow-spacing':'off',
+    'no-sequences':'off',
+    'no-unused-expressions':'off',
+    'no-multi-spaces':'off',
+    'no-unused-vars':'off',
+    'camelcase ':'off',
+    'quote-props':'off'
   }
 }
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import AboutView from '../views/AboutView'
 
 Vue.use(VueRouter)
 
@@ -13,10 +14,7 @@ const routes = [
   {
     path: '/about',
     name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    component: AboutView
   }
 ]
 
