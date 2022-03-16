7fca0f5de87850f562625f35c2eac1c0c9e49603
LoverITer <2489868503@qq.com>
2022-03-16 23:00:49 +0800

    feat: 初始化协议页面

--- src/views/ServiceProtocolView.vue
+++ src/views/ServiceProtocolView.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
-    <h1>服务协议</h1>
+    <v-card class="mx-auto my-12 rounded-lg login-card" max-width="444" height="80%" min-height="500">
+      <v-card-title class="login-title">EasyBlog • <h2>用户协议</h2></v-card-title>
+    </v-card>
   </div>
 </template>
 
