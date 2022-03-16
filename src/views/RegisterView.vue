082d9bc1bae79be649a8218e9db29447c51902a7
LoverITer <2489868503@qq.com>
2022-03-16 21:23:51 +0800

    feat: 增加邮箱号注册页面

--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -1,13 +1,55 @@
 <template>
-  <div><h1>This is an register page</h1>
-
-    <router-link to="/login">返回登录</router-link>
+  <div class="register-container">
+    <v-card class="mx-auto my-12 rounded-lg register-card" max-width="444" height="70%" min-height="500">
+      <v-card-title class="register-title"><h2>邮箱注册</h2></v-card-title>
+      <div class="register-body">
+        <v-card color="basil" flat>
+          <v-card-text>
+            <v-form ref="emailFormRef" lazy-validation>
+              <!--邮箱号-->
+              <v-text-field v-model="email_account.email" label="邮箱" autofocus required></v-text-field>
+              <!--密码-->
+              <v-text-field
+                v-model="email_account.password"
+                label="密码"
+                required
+              ></v-text-field>
+              <!--注册按钮-->
+              <v-btn :color="'#3370FF'" style="color: aliceblue;" width="100%">
+                注册
+              </v-btn>
+              <div class="email-go-login-box">
+                <span>已有账号？<router-link to="/login" class="content">返回登录</router-link></span>
+              </div>
+            </v-form>
+          </v-card-text>
+        </v-card>
+      </div>
+      <!--第三方登录框-->
+      <ThirdPartyLoginBox></ThirdPartyLoginBox>
+      <!--隐私协议-->
+      <div class="privacy_box">
+        <span>登录注册即代表同意EasyBlog <router-link class="content" to="/protocol">《服务协议》</router-link> 和 <router-link
+          class="content" to="/privacy">《用户隐私政策》</router-link></span>
+      </div>
+    </v-card>
   </div>
 </template>
 
 <script>
+import ThirdPartyLoginBox from '@/components/ThirdPartyLoginBox'
+
 export default {
-  name: 'RegisterView'
+  name: 'RegisterView',
+  components: { ThirdPartyLoginBox },
+  data: () => ({
+    register_tab: null,
+    //邮箱账号登录
+    email_account: {
+      email: '',
+      password: ''
+    },
+  })
 }
 </script>
 
