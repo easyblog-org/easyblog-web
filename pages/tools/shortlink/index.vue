e4ee439af3edc88b4ddb40e46283676f25da7fa3
huangxin <2489868503@qq.com>
2024-01-09 20:19:46 +0800

    feat: 在线工具模块开发

---
+++ pages/tools/shortlink/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-app>
+    <div><h1>This is an get back password page</h1></div>
+  </v-app>
+</template>
+
+<script>
+export default {
+name: "ForgetPasswordView"
+}
+</script>
+
+<style scoped>
+
+</style>
